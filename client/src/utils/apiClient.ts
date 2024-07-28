e7913647e6f06ac30d312928b0a0800981e8b817
Tolga <tolgazorlu17@gmail.com>
2024-07-28 15:21:53 +0300

    client changed

--- client/src/utils/apiClient.ts
+++ client/src/utils/apiClient.ts
@@ -2,9 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
     baseURL:
-        process.env.NODE_ENV === "development"
-            ? "http://localhost:8000/v1/"
-            : "/",
+        process.env.NODE_ENV === "production" ? "https://leearn.app/v1/" : "/",
     headers: {
         "Content-Type": "application/json",
     },
